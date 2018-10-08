f7bec284f6d9d78a0f984a6c59b480e02eb757bc
Yves1812 <yves.bonnefont@gmail.com>
2018-10-08 23:06:32 +0200

    I2C methods to read TP attributes

--- Boards/MU - backend/main_unit.py
+++ Boards/MU - backend/main_unit.py
@@ -2,11 +2,15 @@ import sqlite3
 from smbus2 import SMBus
 import logging
 import time
+import datetime
 
 roverdb='C:\\Users\\Yves1812\\Documents\\GitHub\\Robot_tondeuse\\Boards\\MU - web server\\app.db'
-pi_I2C_bus=0
+pi_I2C_bus=1
 TP_board_I2C_address=0x60
-encoder_register=1
+position_register=1
+speed_register=9
+power_register=14
+
 
 #define CMPS12_ADDRESS 0x60  // Address of CMPS12 shifted right one bit for arduino wire library
 #define ANGLE_8  1           // Register to read 8bit angle from
@@ -22,6 +26,8 @@ class Device(object):
         specified I2C bus number."""
         self._address = address
         self._bus = SMBus(busnum)
+        self._logger = logging.getLogger('Adafruit_I2C.Device.Bus.{0}.Address.{1:#0X}'.format(busnum, address))
+
 
     def writeRaw8(self, value):
         """Write an 8-bit value on the bus (without register)."""
@@ -47,29 +53,25 @@ class Device(object):
     def writeList(self, register, data):
         """Write bytes to the specified register."""
         self._bus.write_i2c_block_data(self._address, register, data)
-        self._logger.debug("Wrote to register 0x%02X: %s",
-                     register, data)
+        self._logger.debug("Wrote to register 0x%02X: %s", register, data)
 
     def readList(self, register, length):
         """Read a length number of bytes from the specified register.  Results
         will be returned as a bytearray."""
         results = self._bus.read_i2c_block_data(self._address, register, length)
-        self._logger.debug("Read the following from register 0x%02X: %s",
-                     register, results)
+        self._logger.debug("Read the following from register 0x%02X: %s", register, results)
         return results
 
     def readRaw8(self):
         """Read an 8-bit value on the bus (without register)."""
         result = self._bus.read_byte(self._address) & 0xFF
-        self._logger.debug("Read 0x%02X",
-                    result)
+        self._logger.debug("Read 0x%02X",result)
         return result
 
     def readU8(self, register):
         """Read an unsigned byte from the specified register."""
         result = self._bus.read_byte_data(self._address, register) & 0xFF
-        self._logger.debug("Read 0x%02X from register 0x%02X",
-                     result, register)
+        self._logger.debug("Read 0x%02X from register 0x%02X",result, register)
         return result
 
     def readS8(self, register):
@@ -84,8 +86,7 @@ class Device(object):
         specified endianness (default little endian, or least significant byte
         first)."""
         result = self._bus.read_word_data(self._address,register) & 0xFFFF
-        self._logger.debug("Read 0x%04X from register pair 0x%02X, 0x%02X",
-                           result, register, register+1)
+        self._logger.debug("Read 0x%04X from register pair 0x%02X, 0x%02X", result, register, register+1)
         # Swap bytes if using big endian because read_word_data assumes little
         # endian on ARM (little endian) systems.
         if not little_endian:
@@ -122,12 +123,15 @@ class Device(object):
         return self.readS16(register, little_endian=False)
 
 class Rover(object):
-    def __init__(self,x=0.0,y=0.0,speedx=0.0,speedy=0.0,teta_point=0,ice_status=False, battery_level=None,traction_power=None, messages=[]):
+    def __init__(self,x=0.0,y=0.0,speedx=0.0,speedy=0.0,teta_point=0,bearing8=0,ice_status=False, battery_level=None,traction_power=None, messages=[]):
         self.x=x
         self.y=y
+
         self.vx=speedx
         self.vy=speedy
+        self.bearing8=bearing8
         self.teta_point=teta_point
+        
         self.ice_status=ice_status
         self.battery_level=battery_level
         self.traction_power=traction_power
@@ -136,13 +140,6 @@ class Rover(object):
         self.conn.row_factory = sqlite3.Row
         self.cursor=self.conn.cursor()
         self.TP_board=Device(TP_board_I2C_address,pi_I2C_bus) # need to put here the right bus number
-
-        self.bearing8=0
-        self.bearing16=0
-        self.pitch=0
-        self.roll=0            
-
-
 
     def query_db_status(self):
         self.cursor.execute("select * from traction_status order by timestamp desc")
@@ -159,40 +156,31 @@ class Rover(object):
 ##            self.messages=latest['messages']
         
     def save_rover_status(self):
-        values=(self.x, self.y,self.vx, self.vy, self.ice_status,self.battery_level,self.traction_power)##self.messages
+        values=(self.x, self.y,self.vx, self.vy,self.bearing8,self.teta_point,self.ice_status,self.battery_level,self.traction_power)##self.messages
         self.cursor.execute("""insert into traction_status (x, y,vx,vy,ice_status, battery_level, power) values (?, ?, ?, ?, ?, ?, ?)""", values)
 
-    def get_rover_status(self, data_type):
-        if (data_type == 0) : # read all
-            print("Data type 0 not implemented yet")
-            
-        elif (data_type == 1) : # read decoder based estimates (x,y,vx,vy,teta_point)
-            length=14
-            decoder_data=TP_board.readList(encoder_register, length)
-            self.x=int.from_bytes(decoder_data[0:3], byteorder='big', signed=True)
-            self.y=int.from_bytes(decoder_data[4:7], byteorder='big', signed=True)
-            self.vx=int.from_bytes(decoder_data[8:9], byteorder='big', signed=True)
-            self.vy=int.from_bytes(decoder_data[10:11], byteorder='big', signed=True)
-            self.teta_point=int.from_bytes(decoder_data[12:13], byteorder='big', signed=True)
-            
-        elif (data_type == 2) : # read GPS based estimates (x,y,vx,vy)
-            print("Data type 2 not implemented yet")
+    def get_rover_position(self):
+        length=9
+        decoder_data=TP_board.readList(position_register, length)
+        self.x=(((decoder_data[0]*256+decoder_data[1])*256+decoder_data[2])*256+decoder_data[3]
+        self.y=(((decoder_data[4]*256+decoder_data[5])*256+decoder_data[6])*256+decoder_data[7]
+        self.bearing8=decoder_data[8]
+##        self.x=int.from_bytes(decoder_data[0:3], byteorder='big', signed=True)
+##        self.y=int.from_bytes(decoder_data[4:7], byteorder='big', signed=True)
 
-        elif (data_type == 3) : # read other data (power, ice_status)
-            print("Data type 3 not implemented yet")
 
-        elif (data_type == 4) : # read messages
-            print("Data type 4 not implemented yet")
+    def get_rover_speed(self):
+        length=6
+        decoder_data=TP_board.readList(speed_register, length)
+        self.vx=decoder_data[0]*256+decoder_data[1]
+        self.vy=decoder_data[2]*256+decoder_data[3]
+        self.teta_point=decoder_data[4]*256+decoder_data[5]
 
-        elif (data_type == 5) : # test with compass sensor
-            length=5
-            compass_data=TP_board.readList(encoder_register, length)
-            self.bearing8=int.from_bytes(decoder_data[0], byteorder='big', signed=True)
-            self.bearing16=int.from_bytes(decoder_data[1:2], byteorder='big', signed=True)
-            self.pitch=int.from_bytes(decoder_data[3], byteorder='big', signed=True)
-            self.roll=int.from_bytes(decoder_data[4], byteorder='big', signed=True)
-        else :
-            print("Unknown datatype to be red from TP board")
+    def get_rover_power(self):
+        length=3
+        decoder_data=TP_board.readList(power_register, length)
+        self.power=decoder_data[0]*256+decoder_data[1]
+        self.ice_status=decoder_data[2]
 
     def set_routing(self):
         pass
@@ -205,15 +193,20 @@ class Rover(object):
 
 print("Welcome to my rover")
 
-myrover=rover()
+myrover=Rover()
 ##myrover.query_db_status()
 ##print('x=',myrover.x)
 ##print('y=',myrover.y)
 
-for i in range(1000):
-    myrover.get_rover_status(5)
-    print(myrover.bearing8)
-    print(myrover.bearing16)
-    print(myrover.pitch)
-    print(myrover.roll)
-    time.sleep(1)
+
+print(datetime.datetime.now())
+for i in range(10000):
+    myrover.get_rover_status(0)
+print(datetime.datetime.now())
+
+##    print("Bearing 8: ",myrover.bearing8*360/255)
+##    print("Bearing 16: ", myrover.bearing16/10)
+##    print("pitch: ",myrover.pitch)
+##    print("Roll: ",myrover.roll)
+##    print("")
+#    time.sleep(10)
