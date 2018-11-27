8e3c1a06dff040c13b9b46544ee6a2fa5d468bcb
Yves1812 <Yves1812@users.noreply.github.com>
2018-11-27 20:23:36 +0100

    Front end updates

--- Boards/MU - web server/app/__init__.py
+++ Boards/MU - web server/app/__init__.py
@@ -4,10 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 
+UPLOAD_FOLDER = '/path/to/the/uploads'
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from app import routes, models
--- Boards/MU - web server/app/routes.py
+++ Boards/MU - web server/app/routes.py
@@ -4,6 +4,9 @@ from app.forms import LoginForm
 from app.models import TractionStatus
 from werkzeug import secure_filename
 
+ALLOWED_EXTENSIONS = set(['rte'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 @app.route('/index')
@@ -26,14 +29,43 @@ def traction_status():
     if status is not None:
         parameters=dict((col, getattr(status[0],col)) for col in status[0].__table__.columns.keys())
     return render_template('traction_status.html', title='Traction Status', parameters=parameters)
+    
+# Need to have this a page + then share the active file back with traction_status and let the backend know
+@app.route('/', defaults={'req_path': ''}) #catches req_path not needed here
+@app.route('/<path:req_path>')
+def dir_listing(req_path):
+    BASE_DIR = '/Users/vivek/Desktop'
 
-@app.route('/load_route')
-def load_route():
-    return render_template('load_route.html', title='Load Route')
+    # Joining the base and the requested path
+    abs_path = os.path.join(BASE_DIR, req_path)
 
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+
+    # Show directory contents
+    files = os.listdir(abs_path)
+    return render_template('files.html', files=files)
+
+@app.route('/upload_route', methods = ['GET', 'POST'])
+def upload_route():
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+    		flash('No file part')
+	    	return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit an empty part without filename
+		if file.filename == '':
+		    flash('No selected file')
+		    return redirect(request.url)
+		if file and allowed_file(file.filename):
+		    filename = secure_filename(file.filename)
+		    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		return redirect(url_for('traction_status')
+    return render_template('upload_route.html', title='Upload Route')
